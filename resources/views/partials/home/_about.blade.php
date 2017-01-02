5730e7586528d0a5e1eda6cba6f56b925b7f4841
Rizqy Faishal <rizqyfaishal@hotmail.com>
2017-01-03 02:02:04 +0700

    add responsive design

--- resources/views/partials/home/_about.blade.php
+++ resources/views/partials/home/_about.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="columns eleven offset-by-half">
-                <p><span class="bold">Sailfish Aquatic Club (SAC)</span> merupakan kursus renang pertama yang menggabungkan konsep
+                <p class="text-center"><span class="bold">Sailfish Aquatic Club (SAC)</span> merupakan kursus renang pertama yang menggabungkan konsep
                     sekolah renang (SAC Swimming School), terapi kesehatan media renang (SAC Swimming Fit),
                     dan klub renang (SAC Swimming Pro) dengan kurikulum yang berbasis US Swimming School,
                     British Swimming School, dan Canadian Red Cross. Kami didukung oleh instruktur (Trainer) berkualitas yang telah berprestasi di tingkat nasional
@@ -12,19 +12,52 @@
                     (Skill) individu.</p>
                 <h4 class="text-center">Visi</h4>
                 <span class="stripped-center blue small"></span>
-                <p>Menginspirasi setiap individu melalui renang dan menciptakan iklim pembelajaran yang
+                <p class="text-center quote">Menginspirasi setiap individu melalui renang dan menciptakan iklim pembelajaran yang
                     kreatif, peduli, inovatif serta melahirkan generasi atlet yang mandiri, sportif, dan kompetitif.</p>
-                <h4 class="text-center">Misi</h4>
-                <span class="stripped-center blue small"></span>
-                <ul>
-                    <li>Memberikan fasilitas dan pelayanan berkualitas yang didukung oleh program berstandar
-                        Internasional.</li>
-                    <li>Menciptakan media pembelajaran yang disiplin, kondusif, dan semangat menjiwai
-                        renang untuk mengoptimalkan performa terbaiknya.</li>
-                    <li>Berkomitmen dalam memperkuat aspek keselamatan dan menciptakan lingkungan yang
-                        positif untuk seluruh anggota SAC.</li>
-                    <li>Menanamkan mental yang positif, berjiwa kepemimpinan, dan saling menghargai.</li>
-                </ul>
+
+            </div>
+        </div>
+    </div>
+    <div class="visi">
+        <h4 class="text-center">Misi</h4>
+        <span class="stripped-center blue small"></span>
+        <div class="container">
+            <div class="row">
+                <div class="columns three circle-div">
+                    <div class="circle-block">
+                        <h1 class="text-center">1</h1>
+                    </div>
+                    <p class="text-center">
+                        Memberikan fasilitas dan pelayanan berkualitas yang didukung oleh program berstandar
+                        Internasional.
+                    </p>
+                </div>
+                <div class="columns three circle-div">
+                    <div class="circle-block">
+                        <h1 class="text-center">2</h1>
+                    </div>
+                    <p class="text-center">
+                        Menciptakan media pembelajaran yang disiplin, kondusif, dan semangat menjiwai
+                        renang untuk mengoptimalkan performa terbaiknya.
+                    </p>
+                </div>
+                <div class="columns three circle-div">
+                    <div class="circle-block">
+                        <h1 class="text-center">3</h1>
+                    </div>
+                    <p class="text-center">
+                        Berkomitmen dalam memperkuat aspek keselamatan dan menciptakan lingkungan yang
+                        positif untuk seluruh anggota SAC.
+                    </p>
+                </div>
+                <div class="columns three circle-div">
+                    <div class="circle-block">
+                        <h1 class="text-center">4</h1>
+                    </div>
+                    <p class="text-center">
+                        Menanamkan mental yang positif, berjiwa kepemimpinan, dan saling menghargai.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/partials/home/_programs.blade.php
+++ resources/views/partials/home/_programs.blade.php
@@ -3,30 +3,38 @@
     <span class="stripped-center blue"></span>
     <div id="pricing-table" class="clear">
         <div class="plan">
-            <h3>SAC Swimming Fit<span>$59</span></h3>
+            <h3>SAC Swimming School<span>$59</span></h3>
             <a class="button button-primary" href="">Learn More</a>
             <ul>
-                <li><b>10GB</b> Disk Space</li>
+                <li class="bold">Program sekolah renang yang berbasis kurikulum</li>
+                <li>Kelas Privat</li>
+                <li>Kelas Semi Privat</li>
+                <li>Kelas Group / Kelompok</li>
+                <li>Program Ekstrakurikuler</li>
+                <li>Program Pembinaan O2SN (Olimpiade Olahraga Siswa Nasional)</li>
             </ul>
         </div>
         <div class="plan" id="most-popular">
-            <h3>SAC Swimming School<span>$29</span></h3>
+            <h3>SAC Swimming Fit<span>$29</span></h3>
             <a class="button button-primary" href="">Learn More</a>
             <ul>
-                <li><b>5GB</b> Disk Space</li>
-                <li><b>50GB</b> Monthly Bandwidth</li>
-                <li><b>10</b> Email Accounts</li>
-                <li><b>Unlimited</b> subdomains</li>
+                <li class="bold">Program kebugaran dan penyembuhan dengan media renang</li>
+                <li>Aquatic Physical Therapy</li>
+                <li>Adapted Swimming Treatment</li>
             </ul>
         </div>
         <div class="plan">
             <h3>SAC Swimming Pro<span>$9</span></h3>
             <a class="button button-primary" href="">Learn More</a>
             <ul>
-                <li><b>1GB</b> Disk Space</li>
-                <li><b>10GB</b> Monthly Bandwidth</li>
-                <li><b>2</b> Email Accounts</li>
-                <li><b>Unlimited</b> subdomains</li>
+                <li class="bold">Program kekhususan untuk atlet – atlet yang ingin berlomba di tingkat nasional dengan menu
+                    latihan yang berstandar internasional.</li>
+                <li>KU senior (> 19 Tahun)</li>
+                <li>KU I (16 – 18 Tahun)</li>
+                <li>KU II (14 – 15 Tahun)</li>
+                <li>KU III (12 – 13 Tahun)</li>
+                <li>KU IV (10 – 11 Tahun)</li>
+                <li>KU V (< 9 Tahun)</li>
             </ul>
         </div>
     </div>
